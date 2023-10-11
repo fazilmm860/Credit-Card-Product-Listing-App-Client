f6aa0315539ea98fd869537b3cab65893a383cb7
Mohamed Fazil M <fazilmm860@gmail.com>
2023-10-11 12:16:07 +0530

    Tailwind config the Navbar is created, The page are created and routes are defiend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 
 import './App.css';
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Navbar from './pages/Navbar'
+import HomePage from './pages/HomePage';
+import Categories from './pages/Categories';
+import AddCards from './pages/AddCards';
 function App() {
   return (
-    <div className="App">
-      Credit Card Product List App
-    </div>
+    <>
+    <Navbar/>
+    <BrowserRouter>
+    <Routes>
+      <Route
+      path='/dash'
+      element={
+        <HomePage/>
+      }
+      />
+       <Route
+      path='/category'
+      element={
+        <Categories/>
+      }
+      />
+ <Route
+      path='/addCards'
+      element={
+        <AddCards/>
+      }
+      />
+      
+    </Routes>
+    
+    </BrowserRouter>
+
+    </>
+    
   );
 }
 
---
+++ src/pages/AddCards.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddCards = () => {
+  return (
+    <div>
+      Add Credit cards
+    </div>
+  )
+}
+
+export default AddCards
